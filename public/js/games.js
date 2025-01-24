a65cd6f225585c26ce1ebd20a33961cedacc7c25
RAX <astraltechh@gmail.com>
2025-01-24 09:56:36 -0600

    Initial commit

---
+++ public/js/games.js
@@ -0,0 +1,18 @@
+fetch('json/games.json')
+    .then(response => response.json())
+    .then(games => {
+        const buttonContainer = document.getElementById('button-container');
+        games.forEach(game => {
+            const gameButton = document.createElement('a');
+            gameButton.href = game.location;
+            gameButton.className = 'game-button';
+            gameButton.innerHTML = `
+                <img src="${game.image}" alt="${game.name}" class="game-image">
+                <br>${game.name}
+            `;
+            buttonContainer.appendChild(gameButton);
+        });
+    })
+    .catch(error => {
+        console.error('Error fetching games.json:', error);
+    });
---
+++ server.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/@', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'games.html'));
+});
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'apps.html'));
+});
+
+app.get('/404', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', '404.html'));
+});
+
+app.use((req, res) => {
+    res.redirect('/404');
+  });
+  
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
