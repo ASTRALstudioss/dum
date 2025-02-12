f9dc6c29da0efd5851bb3a6acafa8acefa903d56
RAX <astraltechh@gmail.com>
2025-02-11 22:25:30 -0600

    dum.
    added game query so I dont have to add the individual game link because of how long they are

--- public/js/apps.js
+++ public/js/apps.js
@@ -5,10 +5,10 @@ fetch('json/apps.json')
         games.forEach(game => {
             const gameButton = document.createElement('a');
             gameButton.href = game.location;
-            gameButton.className = 'game-button';
+            gameButton.className = 'app-button';
             gameButton.innerHTML = `
-                <img src="${game.image}" alt="${game.name}" class="game-image">
-                <div class="game-title">${game.name}</div>
+                <img src="${game.image}" alt="${game.name}" class="app-image">
+                <div class="app-title">${game.name}</div>
             `;
             buttonContainer.appendChild(gameButton);
         });
